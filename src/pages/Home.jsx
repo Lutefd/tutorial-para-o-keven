ab267a20bfd19caab7ef8ffd634aa08a79ccea00
luis dourado <lfaddourado@gmail.com>
2023-04-07 15:54:00 -0300

    Home display

---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import useCounterStore from '../store/counterStore';
+import useDataStore from '../store/dataStore';
+import useUserStore from '../store/userStore';
+
+const Home = () => {
+  const { count, increment, decrement } = useCounterStore();
+  const { data, clearData } = useDataStore();
+  const { user } = useUserStore();
+
+  return (
+    <div className="flex justify-center flex-col items-center">
+      <h1 className="text-4xl">Olá {user}</h1>
+      <p>Quantos amigos você tem?</p>
+      <p className="text-2xl text-center">{count}</p>
+      <div className="flex gap-2">
+        <button
+          className={count < 10 ? 'btn btn-primary' : 'hidden'}
+          onClick={increment}
+          disabled={count > 9 ? true : false}
+        >
+          {' '}
+          {count} + 1{' '}
+        </button>
+        <button
+          className={count > 0 ? 'btn btn-primary' : 'hidden'}
+          onClick={decrement}
+        >
+          {' '}
+          {count} - 1{' '}
+        </button>
+      </div>
+      {data.length > 0 && (
+        <div className="flex flex-col gap-2 justify-center items-center">
+          <h2 className="text-2xl">Lista de amigos</h2>
+          <ul>
+            {data.map((item) => (
+              <li className="text-center" key={item.id}>
+                {item.name}
+              </li>
+            ))}
+          </ul>
+          <button className="btn btn-primary mx-auto " onClick={clearData}>
+            Limpar Dados
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+export default Home;
